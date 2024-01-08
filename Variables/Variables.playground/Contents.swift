21a5624c221b3e6c90958ccdf8a86601223b6044
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2024-01-08 22:59:47 +0300

    Variables example added

---
+++ Variables/Variables.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var ogrenciAdi = "Pervin"
+var ogrenciYasi = 24
+var ogrenciBoy = 1.70
+var ogrenciBasharf = "P"
+
+
+print(ogrenciAdi)
+print(ogrenciYasi)
+print(ogrenciBoy)
+print(ogrenciBasharf)
+
+
+//
+
+var urunId : Int = 3316
+var urunAdi : String = "Saat"
+var urunAdet : Int = 100
+var urunFiyat : Double = 149.99
+var urunTedarikci : String = "apple"
+
+
+print(urunId, urunAdet, urunFiyat,urunAdi ,urunTedarikci)
+
+//String ifade içinde \() ifadesi kullanılarak çıktıya değişken eklenebilir
+
+print("Ürün id :  \(urunId)")
+
+
+print("\(urunTedarikci) marka  \(urunAdi)  \(urunFiyat) fiyatla satılmaktadır. Stokta \(urunAdet) kalmıştır.")
+      
