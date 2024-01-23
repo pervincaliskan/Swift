367b528c376122b79fa50c1a49d4bd0d1bc55dd3
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2024-01-23 23:46:12 +0300

    Arithmetic Operators Example added

---
+++ Basic-Arithmetic Operators/MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+//Daire alanı
+
+let pi = 3.14
+
+let yariCap = 2.0
+
+let alan = pi * yariCap * yariCap
+
+print(alan)
+
+//F = m x a
+
+var m = 12.5
+var a = 10.0
+var f = m * a
+print(f)
+
+// Ax
+var v = 12.7
+var v0 = 23.56
+var t = 3.5
+
+var x1 = ((v+v0)/2)*t
+print(x1)
+
+var x2 = (v0*t) + (a*t*t)/2
+print(x2)
+
+//Kısaltma
+
+
+var y = 10
+
+y = y + 2
+
+print(y)
+
+y+=2
+print(y)
+
+y-=3
+print(y)
+
+y*=4
+print(y)
+
+
+y/=2
+print(y)
