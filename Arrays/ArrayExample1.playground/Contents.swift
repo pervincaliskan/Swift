5f8a2e52b365b02c2342e7d69cf5e2a363836387
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2024-01-30 16:28:20 +0300

    Arrays Example Added

---
+++ Arrays/ArrayExample1.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var ages = [3,5,7,18]
+var name = ["Ali", "Pervin", "Ahmet"]
+print("\(name[0]),\(ages[2]) yaşındadır")
+
+var fruits = ["strawberries", "limes", "tangerines"]
+fruits[1] = "grapes"
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupations["Jayne"] = "Public Relations"
+
+
+fruits.append("blueberries")
+print(fruits)
+
