3b4d90cadc1b6a4d9806f61560f0377d12d0343b
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2024-01-31 00:27:10 +0300

    Array Example Added

---
+++ Arrays/ArrayExample2/ArrayExample2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ArrayExample2
+//
+//  Created by Pervin Çalışkan on 30.01.2024.
+//
+
+import Foundation
+
+var fruits = ["strawberries", "limes", "tangerines"]
+ print(fruits[1])
+
