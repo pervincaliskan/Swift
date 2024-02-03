3cef5d3e2e1792a6c42b70cf6a8e9459655a038a
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2024-02-03 19:33:57 +0300

    Operators Example Added

---
+++ Basic-Arithmetic Operators/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  Operators
+//
+//  Created by Pervin Çalışkan on 3.02.2024.
+//
+
+import Foundation
+
+//Bileşik atama operatörleri
+
+var x = 3
+x += 2
+print(x)
+
+// karşılaştırma operatörleri
+
+print( 7 == 7)
+print(9 < 10)
+print(7 > 3)
+print(8 >= 8)
+print(5 <= 1)
+if "x" == "x"{
+    print(" Eşitlik doğru")
+    
+} else {
+    print("Eşitlik yanlış")
+}
+
+//Koşul operatörleri
+"x" == "x" ? print("Eşitlik doğgru") : print("Eşitlik yanlış")
+
+
