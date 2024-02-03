239a95cb22713d8051bee68a060f0db289068949
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2024-02-04 01:03:19 +0300

    Switch Case Example Added

---
+++ Control Flow/SwitchExample.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+let gun = 9
+
+switch gun {
+    case 1:
+        print("Pazartesi")
+    case 2:
+        print("Salı")
+    case 3:
+        print("Çarşamba")
+    case 4:
+        print("Perşembe")
+    case 5:
+        print("Cuma")
+    case 6:
+        print("Cumartesi")
+    case 7:
+        print("Pazar")
+    default:
+        print("Böyle bir gün yok")
+}
+
+
